139180c0062c5c8bf239582a59755d41bd537e1f
insta-code1 <eoinkane1@gmail.com>
2017-02-22 18:48:11 +0000

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let transcode = (videoFn) => {
 
   let cmd = `mkdir -p ./output/${video}/ && ffmpeg -i ./input/${videoFn} -start_number 0 -hls_time 10 -hls_list_size 0 -strict -2 ./output/${video}/${video}.m3u8`;
 
-  exec(cmd, function(error, stdout, stderr) {
+  exec(cmd, (error, stdout, stderr) => {
     // command output is in stdout
     console.log(stdout);
     console.log(stderr);
